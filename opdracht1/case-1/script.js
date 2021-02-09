3dc22ab997c5cc681c2c4c34546c1ee52bfb4fc1
MiWi98 <74242055+MiWi98@users.noreply.github.com>
2021-02-09 12:18:46 +0100

    kleine aanpassingen.

--- opdracht1/case-1/script.js
+++ opdracht1/case-1/script.js
@@ -3,7 +3,7 @@ var hearts = document.querySelector("button.heart");
 hearts[0].addEventListener("click","dblclick", favorieten)
 
 
-/* toevoegn aan favorieten of weghalen */
+/* toevoegen aan favorieten of weghalen */
 function favorieten(event) {
     /* als het hartje nog niet gefavoriet is */
     if (clickedHeart.innerHTML == "🤍") {
